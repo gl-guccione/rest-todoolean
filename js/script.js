ce83aa6afc88e45536b6a92261e467681385da0b
gl-guccione <g.guccione3@gmail.com>
2020-09-28 15:30:14 +0200

    Add list (read from server)

--- js/script.js
+++ js/script.js
@@ -1,2 +1,42 @@
 // api - port 3023
 // http://157.230.17.132:3023/todos
+
+
+// functions
+
+function displayData(date) {
+
+  var source = $("#todo-list-template").html();
+  var template = Handlebars.compile(source);
+
+  for (var i = 0; i < date.length; i++) {
+    
+    var html = template(date[i]);
+
+    $("#todo-list").append(html);
+  }
+}
+
+
+function getData() {
+  $.ajax(
+    {
+      "url": "http://157.230.17.132:3023/todos",
+      "method": "GET",
+      "success": function(date, state) {
+        displayData(date);
+      },
+      "error": function() {
+        alert("error");
+      },
+    }
+  );
+}
+
+
+// script
+$(document).ready(function() {
+
+  getData();
+
+});
